875c25e71276fecdd7adf38d1d9d83417c7fa965
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 20:19:58 +0000

    Add GroupPage component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/app/group/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { db } from "@/server/db";
+import groupsTable from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+
+type GroupPageProps = {
+  params: { id: string };
+};
+
+export default async function GroupPage({ params }: GroupPageProps) {
+  const groupData = await db
+    .select()
+    .from(groupsTable)
+    .where(eq(groupsTable.id, params.id));
+
+  if (groupData.length === 0) {
+    return <h1>Group not found</h1>;
+  }
+
+  return (
+    <>
+      <h1>Group {params.id}</h1>
+      {groupData.map((group) => {
+        return (
+          <div key={group.id}>
+            <p>Name: {group.name}</p>
+            <p>Description: {group.description}</p>
+            <p>Active: {group.active}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
