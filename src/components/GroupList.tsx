e731b38cb5527658ab93d78b21161532f3c30841
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 13:05:31 +0000

    Refactor GroupList component to use flexbox layout
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -10,13 +10,13 @@ type GroupListProps = {
 
 export function GroupList({ groups }: GroupListProps) {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h2>Group List</h2>
       {/* if list is empty, display a short message instead of a list  */}
-      {groups.length === 0 && <p>No groups found</p>}
+      {groups.length === 0 && <p>No groups found...</p>}
       {/* if list is not empty, display a list of groups */}
       {groups.length > 0 && (
-        <ul>
+        <ul className="flex flex-col gap-2">
           {groups.map((group) => (
             <li key={group.id}>
               <h3>{group.name}</h3>
