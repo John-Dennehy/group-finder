23d37c2152388d8a2f2ce757d45e2c628a76a9d1
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 13:38:42 +0000

    Add revalidation of home page after creating a new group
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/server/actions/new-group-action.ts
+++ src/server/actions/new-group-action.ts
@@ -3,16 +3,18 @@
 import { action } from "@/lib/safe-action-client";
 import groupsTable, { zodInsertGroupSchema } from "../db/schema";
 import { db } from "../db";
+import { revalidatePath } from "next/cache";
 
 const schema = zodInsertGroupSchema;
 
 export const newGroupAction = action(zodInsertGroupSchema, async (newGroup) => {
-  // // pause for 2 seconds to simulate a slow database
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
-
   // ... create new group in database
   db.insert(groupsTable).values(newGroup).execute();
 
+  // ... revalidate the home page
+  revalidatePath("/");
+
+  // ... return success message	on server console
   console.log("newGroupAction", newGroup);
 
   return { success: "Group created" };
