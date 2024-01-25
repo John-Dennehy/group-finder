4fdfa0803ab6d3559a16bce546d9f9be95c9dec0
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 13:21:36 +0000

    Refactor newGroupAction to use groupsTable and db.insert()
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/server/actions/new-group-action.ts
+++ src/server/actions/new-group-action.ts
@@ -1,14 +1,18 @@
 "use server"; // don't forget to add this!
 
 import { action } from "@/lib/safe-action-client";
-import { zodInsertGroupSchema } from "../db/schema";
+import groupsTable, { zodInsertGroupSchema } from "../db/schema";
+import { db } from "../db";
 
 const schema = zodInsertGroupSchema;
 
 export const newGroupAction = action(zodInsertGroupSchema, async (newGroup) => {
+  // // pause for 2 seconds to simulate a slow database
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+
   // ... create new group in database
-  // pause for 2 seconds to simulate a slow database
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  db.insert(groupsTable).values(newGroup).execute();
+
   console.log("newGroupAction", newGroup);
 
   return { success: "Group created" };
