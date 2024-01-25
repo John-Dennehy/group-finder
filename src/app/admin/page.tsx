ef991425866f7fca4fbdb252c60e0411954ba956
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 16:28:18 +0000

    Refactor admin page and rename Home component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,8 @@
 "use server";
 
 import GroupList from "@/components/GroupList";
+import NewGroupForm from "@/components/NewGroupForm";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { db } from "@/server/db";
 import groupsTable from "@/server/db/schema";
 
@@ -9,15 +11,23 @@ export const selectAllGroups = async () => {
   return groups;
 };
 
-export default async function HomePage() {
+export default async function AdminPage() {
   const groups = await selectAllGroups();
 
   return (
     <main className="container mx-auto">
       <div className="flex flex-col gap-4">
-        <h1>Home</h1>
-
-        <GroupList groups={groups} />
+        <h1>Admin Page</h1>
+        <Card>
+          <CardHeader>
+            <h2>Create New Group</h2>
+          </CardHeader>
+          <CardContent>
+            <NewGroupForm />
+          </CardContent>
+        </Card>
+        <hr />
+        <div>Table</div>
       </div>
     </main>
   );
