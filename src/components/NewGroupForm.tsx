4a25969486446e93ff24075a8cbd43dd4a874f5c
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 12:48:01 +0000

    Add NewGroupForm component with form validation
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/NewGroupForm.tsx
+++ src/components/NewGroupForm.tsx
@@ -1,7 +1,82 @@
+"use client";
+
+import groupsTable from "@/server/db/schema";
+import { createInsertSchema } from "drizzle-zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "./ui/checkbox";
+
+const zodInsertGroupSchema = createInsertSchema(groupsTable, {
+  // We can add additional validation here.
+  // For example, we can check that the username is unique.
+  name: z
+    .string()
+    .min(4, "Group name must be at least 4 characters long")
+    .max(16, "Group name must be no more then 16 characters long"),
+  active: z.boolean().default(false),
+});
+
 export default function NewGroupForm() {
+  const form = useForm<z.infer<typeof zodInsertGroupSchema>>({
+    resolver: zodResolver(zodInsertGroupSchema),
+  });
+
+  function handleValidSubmit(data: z.infer<typeof zodInsertGroupSchema>) {
+    alert(`Valid: ${JSON.stringify(data)}`);
+  }
+
   return (
     <>
       <h1>NewGroupForm</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleValidSubmit)}
+          className="space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Group Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Group name..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="active"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Active</FormLabel>
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </>
   );
 }
