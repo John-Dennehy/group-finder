9c7a94f28ce9ea18f015c6210516c6f6192b5203
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 22:03:28 +0000

    Add DrawerDialog component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/components/drawer-dialog.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import { useMediaQuery } from "@/lib/hooks/use-media-query";
+import { ReactNode, useState } from "react";
+
+type DrawerDialogProps = {
+  triggerButton?: ReactNode;
+  title: string;
+  description?: string;
+  form?: ReactNode;
+};
+
+export function DrawerDialog({
+  title,
+  triggerButton,
+  description,
+  form,
+}: DrawerDialogProps) {
+  const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          {triggerButton ? (
+            triggerButton
+          ) : (
+            <Button variant="outline">{title}</Button>
+          )}
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            {description && (
+              <DialogDescription>{description}</DialogDescription>
+            )}
+          </DialogHeader>
+          {form && form}
+        </DialogContent>
+      </Dialog>
+    );
+  }
+  if (!isDesktop) {
+    return (
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          {triggerButton ? (
+            triggerButton
+          ) : (
+            <Button variant="outline">{title}</Button>
+          )}
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader className="text-left">
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <div className="m-4">{form && form}</div>
+          <DrawerFooter className="pt-2">
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return null;
+}
