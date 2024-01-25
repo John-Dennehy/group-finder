133acf3e4a6471afaf9873f29f2abb6d626ad900
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 18:11:56 +0000

    Add date-time formatting for columns in group table
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/group-table/columns.tsx
+++ src/components/group-table/columns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, Row, Cell } from "@tanstack/react-table";
 import { GroupSelect } from "@/server/db/schema";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import { Button } from "../ui/button";
@@ -30,15 +30,52 @@ export const columns: ColumnDef<Group>[] = [
   {
     accessorKey: "createdAt",
     header: ({ column }) => <SortButton column={column}>Created At</SortButton>,
+    cell: (cell) => {
+      const dateValue = cell.row.original.createdAt
+        ? new Date(cell.row.original.createdAt)
+        : null;
+      return <DateTimeRow dateValue={dateValue} />;
+    },
   },
   {
     accessorKey: "verifiedAt",
     header: ({ column }) => (
       <SortButton column={column}>Verified At</SortButton>
     ),
+    cell: (cell) => {
+      const dateValue = cell.row.original.verifiedAt
+        ? new Date(cell.row.original.verifiedAt)
+        : null;
+      return <DateTimeRow dateValue={dateValue} />;
+    },
   },
   {
     accessorKey: "updatedAt",
     header: ({ column }) => <SortButton column={column}>Updated At</SortButton>,
+    cell: (cell) => {
+      const dateValue = cell.row.original.updatedAt
+        ? new Date(cell.row.original.updatedAt)
+        : null;
+      return <DateTimeRow dateValue={dateValue} />;
+    },
   },
 ];
+
+type RowDateTimeProps = {
+  dateValue: Date | null;
+};
+
+export function DateTimeRow({ dateValue: rowValue }: RowDateTimeProps) {
+  if (!rowValue) {
+    return <div className="font-medium text-center">-</div>;
+  }
+  const formattedDate = formatDateTime(rowValue);
+  return <div className="font-medium text-left">{formattedDate}</div>;
+}
+
+export function formatDateTime(date: Date) {
+  return new Intl.DateTimeFormat(["en-GB", "en-US"], {
+    dateStyle: "medium",
+    timeStyle: "short",
+  }).format(date);
+}
