058df6caa25269db0e8f54b23cf4a2ad20569d2c
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 15:59:31 +0000

    Add CardHeader and CardFooter to GroupList component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,5 +1,5 @@
 import groupsTable, { GroupSelect } from "@/server/db/schema";
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 
 type GroupListProps = {
   groups: GroupSelect[];
@@ -17,12 +17,27 @@ export function GroupList({ groups }: GroupListProps) {
           {groups.map((group) => (
             <li key={group.id}>
               <Card>
-                <CardContent>
-                  <h3 className="flex flex-row gap-2 ">
+                <CardHeader>
+                  <h3 className="flex flex-row gap-2 justify-between text-xl">
                     {group.name}
-                    {group.active && <span>✅</span>}
+                    <span className="text-gray-500 text-sm">
+                      {"Active: "}
+                      {group.active && (
+                        <span className="text-green-500 text-xl">✓</span>
+                      )}
+                      {!group.active && <span className=" text-xl">✗</span>}
+                    </span>
                   </h3>
+                </CardHeader>
+
+                <CardContent>
+                  <p>{group.description}</p>
                 </CardContent>
+                <CardFooter>
+                  <p className="italic text-sm font-light text-gray-500 text-right w-full">
+                    id: {group.id}
+                  </p>
+                </CardFooter>
               </Card>
             </li>
           ))}
