c43da9d42860de1d7690820a3a7958d74f496ee4
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 20:19:40 +0000

    Refactor group-table.tsx to use GroupSelect type
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/group-table/group-table.tsx
+++ src/components/group-table/group-table.tsx
@@ -1,11 +1,11 @@
 "use server";
 
 import { db } from "@/server/db";
-import groupsTable from "@/server/db/schema";
-import { Group, columns } from "./columns";
+import groupsTable, { GroupSelect } from "@/server/db/schema";
+import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
-async function getData(): Promise<Group[]> {
+async function getData(): Promise<GroupSelect[]> {
   const groups = await db.select().from(groupsTable);
   return groups;
 }
