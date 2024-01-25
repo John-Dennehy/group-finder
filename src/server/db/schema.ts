a833adcc5fceba067d2ed36bd90fa5256cc73bbf
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-25 12:32:19 +0000

    Update groupsTable schema with serial id
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,16 @@
-import { boolean, varchar, mysqlTableCreator } from "drizzle-orm/mysql-core";
+import {
+  boolean,
+  varchar,
+  mysqlTableCreator,
+  serial,
+} from "drizzle-orm/mysql-core";
 
 // add prefix for multi-project schema in single Planetscale database
 const prefixedMySqlTable = mysqlTableCreator((name) => `testing_${name}`);
 
 // drizzle schema for groups table
 export const groupsTable = prefixedMySqlTable("groups", {
-  id: varchar("id", { length: 6 }).primaryKey(),
+  id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
   active: boolean("active").default(false).notNull(),
 });
