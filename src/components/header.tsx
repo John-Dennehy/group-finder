14ea1f2cda030737d446a234b3c21f0f96a67a7d
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-26 18:18:53 +0000

    Add header component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/components/header.tsx
@@ -0,0 +1,55 @@
+import { HeartHandshakeIcon } from "lucide-react";
+import Link from "next/link";
+
+type HeaderProps = {
+  title: string;
+};
+
+export default function Header({ title }: HeaderProps) {
+  return (
+    <>
+      <header className=" bg-primary text-primary-foreground">
+        <div className="container mx-auto">
+          <div className="flex items-center justify-between py-4">
+            <div className="flex items-center gap-4 m-0">
+              <HeartHandshakeIcon className="h-10 w-10" />
+              <h1 className="text-xl font-bold">Group Finder</h1>
+            </div>
+            <nav className="hidden lg:flex gap-2">
+              <Link className="hover:underline" href="#">
+                Suggest new Group
+              </Link>
+              <Link className="hover:underline" href="#">
+                Login
+              </Link>
+            </nav>
+            {/* <Sheet className="lg:hidden">
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon className="h-6 w-6" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <div className="grid gap-2 py-6">
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="#"
+              >
+                Suggest new Group
+              </Link>
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="#"
+              >
+                Login
+              </Link>
+            </div>
+          </SheetContent>
+        </Sheet> */}
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
