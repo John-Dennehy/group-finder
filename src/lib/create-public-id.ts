c2f5a015f521f130ddc1939e075dd50a71e81372
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-26 15:33:18 +0000

    Update DEFAULT_LENGTH to 7 in createPublicId()
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/lib/create-public-id.ts
+++ src/lib/create-public-id.ts
@@ -1,7 +1,7 @@
 import { customAlphabet } from "nanoid/non-secure";
 
 const VALID_CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyz";
-const DEFAULT_LENGTH = 6;
+const DEFAULT_LENGTH = 7;
 
 export function createPublicId() {
   const nanoId = customAlphabet(VALID_CHARACTERS, DEFAULT_LENGTH);
