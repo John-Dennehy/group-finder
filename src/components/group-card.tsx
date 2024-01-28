bc7238b28cd6bc4bb03a6ea43c3912f9b3c73a94
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-28 13:11:56 +0000

    Add attendee types list to group card
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/group-card.tsx
+++ src/components/group-card.tsx
@@ -1,10 +1,45 @@
 import { GroupSelect } from "@/server/db/schema";
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { AttendeeTypesList } from "./attendee-types-list";
 
 type GroupCardProps = {
   group: GroupSelect;
 };
+
+export type AttendeeType = {
+  name: string;
+  description?: string;
+};
+
+const defaultAttendeeTypes: AttendeeType[] = [
+  {
+    name: "Parent",
+    description: "For parents with children under 5 years old",
+  },
+  {
+    name: "Toddlers",
+    description: "Children aged 1 to 5 years old",
+  },
+];
+
 export function GroupCard({ group }: GroupCardProps) {
+  // TODO: Data not yet available
+  const currentAddress =
+    "Staines Congregational Church Hall, Stainash Crescent, Staines TW18 1AY";
+
+  const contactDetails = {
+    facebook: "https://www.facebook.com/NCTSAE",
+    instagram: "https://www.instagram.com/nct_staines_ashford_egham/",
+    twitter: "https://twitter.com/NCT_SAE",
+    facebookGroup: "https://www.facebook.com/groups/1234567890",
+    website:
+      "https://www.nct.org.uk/local-activities-meet-ups/region-south-east-england/staines-ashford-egham-surrounding-areas",
+    email: "stainesashfordegham@nct.org.uk",
+    phone: "01234 567890",
+  };
+
+  const attendeeTypes = defaultAttendeeTypes;
+
   return (
     <Card>
       <CardHeader>
@@ -20,7 +55,9 @@ export function GroupCard({ group }: GroupCardProps) {
 
       <CardContent>
         <p>{group.description}</p>
+        <AttendeeTypesList attendeeTypes={attendeeTypes} />
       </CardContent>
+
       <CardFooter>
         <p className="italic text-sm font-light text-gray-500 text-right w-full">
           id: {group.id}
@@ -29,3 +66,4 @@ export function GroupCard({ group }: GroupCardProps) {
     </Card>
   );
 }
+
