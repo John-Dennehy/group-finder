6e34269dc88772467df476606b27a97e3ad7b949
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-28 13:11:46 +0000

    Add AttendeeTypesList component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/components/attendee-types-list.tsx
@@ -0,0 +1,33 @@
+import { AttendeeType } from "./group-card";
+import { Badge } from "./ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+type AttendeeTypesListProps = {
+  attendeeTypes: AttendeeType[];
+};
+
+export function AttendeeTypesList({ attendeeTypes }: AttendeeTypesListProps) {
+  return (
+    <TooltipProvider>
+      <ul className="flex flex-row flex-wrap gap-2">
+        {attendeeTypes.map((type) => (
+          <li key={type.name}>
+            <Tooltip>
+              <TooltipTrigger>
+                <Badge>{type.name}</Badge>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{type.description}</p>
+              </TooltipContent>
+            </Tooltip>
+          </li>
+        ))}
+      </ul>
+    </TooltipProvider>
+  );
+}
