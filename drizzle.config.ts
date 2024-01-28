22d7118fd4bf4be48e36a72996ea759ca627b7fa
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-01-28 16:29:04 +0000

    Update drizzle.config.ts and package.json
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -14,10 +14,9 @@ export default defineConfig({
   dbCredentials: {
     uri: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DB_NAME}?ssl={"rejectUnauthorized":true}`,
   },
-  tablesFilter: [process.env.DB_TABLE_PREFIX as string],
-  schemaFilter: [process.env.DB_TABLE_PREFIX as string],
   strict: true,
-  verbose: true,
+  verbose: false,
   introspect: { casing: "camel" },
   breakpoints: true,
+  tablesFilter: [`${process.env.DB_TABLE_PREFIX}_*`],
 });
