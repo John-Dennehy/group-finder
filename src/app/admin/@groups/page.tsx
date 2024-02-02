90299344c9d4c81f413efe6ebeff2405bfd2c06c
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-02 15:09:46 +0000

    Refactor group creation form
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/app/admin/@groups/page.tsx
+++ src/app/admin/@groups/page.tsx
@@ -1,18 +1,16 @@
 "use server";
 
 import { DrawerDialog } from "@/components/drawer-dialog";
+import GroupMultiForm from "@/components/group-form/group-multi-form";
 import { GroupTable } from "@/components/group-table";
-import NewGroupForm from "@/components/new-group-form";
 
 export default async function GroupAdminPage() {
   return (
     <>
       <div className="flex flex-col gap-4 py-4">
-        <DrawerDialog
-          title="Create New Group"
-          description="Create a new group"
-          form={<NewGroupForm />}
-        />
+        <DrawerDialog title="Create New Group" description="Create a new group">
+          <GroupMultiForm />
+        </DrawerDialog>
 
         <hr />
         <GroupTable />
