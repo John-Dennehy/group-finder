123202a04b0882e4c790c4ca5ad2fe3808f001eb
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-04 21:58:37 +0000

    Refactor GroupMultiForm component to include Group ID and improve UI
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/group-form/group-multi-form.tsx
+++ src/components/group-form/group-multi-form.tsx
@@ -1,22 +1,39 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { createPublicId } from "@/lib/create-public-id";
+import { useEffect, useState } from "react";
 import NewGroupForm from "./new-group-form";
 import { NewScheduleForm } from "./new-schedule-form";
 
 export default function GroupMultiForm() {
+
+  const [groupId, setGroupId] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const publicId = createPublicId();
+    setGroupId(publicId);
+  }, []);
+
   return (
-    <div>
-      <Tabs defaultValue="group">
-        <TabsList>
-          <TabsTrigger value="group">Group</TabsTrigger>
-          <TabsTrigger value="schedule">Schedule</TabsTrigger>
-        </TabsList>
-        <TabsContent value="group">
-          <NewGroupForm />
-        </TabsContent>
-        <TabsContent value="schedule">
-          <NewScheduleForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <div>
+        {groupId && <p>Group ID: {groupId}</p>}
+        <Tabs defaultValue="group">
+          <TabsList>
+            <TabsTrigger value="group">Group</TabsTrigger>
+            <TabsTrigger value="schedule">Schedule</TabsTrigger>
+          </TabsList>
+          <div className="min-h-[300px] ">
+            <TabsContent value="group">
+              <NewGroupForm groupId={groupId} />
+            </TabsContent>
+            <TabsContent value="schedule">
+              <NewScheduleForm groupId={groupId} />
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </>
   );
 }
