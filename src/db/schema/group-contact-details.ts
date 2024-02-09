cae08e37b0efc514f2a5f20e4ca717b093167c50
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2024-02-09 12:03:48 +0000

    Add group contact details schema
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/db/schema/group-contact-details.ts
@@ -0,0 +1,62 @@
+import { relations, sql } from "drizzle-orm";
+import { datetime, int, mysqlEnum, varchar } from "drizzle-orm/mysql-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { prefixedMySqlTable } from "../prefixedMySqlTable";
+import { groupsTable } from "./groups";
+
+const contactTypes = [
+  "email",
+  "phone",
+  "website",
+  "facebook",
+  "twitter",
+  "instagram",
+] as const;
+
+// drizzle schema for group-contact-details table
+export const groupContactDetailsTable = prefixedMySqlTable(
+  "group_contact_details",
+  {
+    id: int("int").autoincrement().primaryKey(),
+    groupId: varchar("group_id", { length: 7 }).notNull(), //.references(() => groupsTable.id),
+    contactType: mysqlEnum("contact_type", contactTypes).notNull(),
+    contact: varchar("contact", { length: 255 }).notNull(),
+    createdAt: datetime("created_at").default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: datetime("updated_at").default(
+      sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`,
+    ),
+  },
+);
+
+// relations (many to one)
+export const groupContactDetailsRelations = relations(
+  groupContactDetailsTable,
+  ({ one }) => ({
+    group: one(groupsTable, {
+      fields: [groupContactDetailsTable.groupId],
+      references: [groupsTable.id],
+    }),
+  }),
+);
+
+// zod insert schema for group-contact-details table
+export const zodInsertGroupContactDetailsSchema = createInsertSchema(
+  groupContactDetailsTable,
+  {
+    // startTime: z.string().regex(timeRegexp),
+  },
+);
+
+// zod select schema for group-contact-details table
+export const zodSelectGroupContactDetailsSchema = createSelectSchema(
+  groupContactDetailsTable,
+);
+
+// typescript types for groups table
+export type GroupContactDetails = typeof groupContactDetailsTable.$inferSelect;
+export type NewGroupContactDetails =
+  typeof groupContactDetailsTable.$inferInsert;
+export type UpdateGroupContactDetails = Required<
+  Pick<GroupContactDetails, "id">
+> &
+  Partial<Omit<GroupContactDetails, "id">>;
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,3 +1,4 @@
 export * from "./groups";
 export * from "./group-schedules";
 export * from "./group-locations";
+export * from "./group-contact-details";
